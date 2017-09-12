305ba7524c6bcd4912452ba5ac96691a42488322
Andrew Laing <parisianconnections@gmail.com>
2017-09-12 09:22:23 +0100

    Update BayesianClassifier.java

--- BayesianClassifier.java
+++ BayesianClassifier.java
@@ -1,7 +1,8 @@
 /**
- * Author: Andrew Laing
- * Email:  parisianconnections@gmail.com
- * Date:   23/12/2016.
+ * Filename: BayesianClassifier.java
+ * Author:   Andrew Laing
+ * Email:    parisianconnections@gmail.com
+ * Date:     23/12/2016.
  */
 
 import java.util.List;
